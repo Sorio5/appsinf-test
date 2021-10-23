e8e47c39bda2b7a777a2d680389e869f760d2bf2
Nabil <nabil.bourhanzour@student.uclouvain.be>
2021-10-24 01:29:32 +0200

    Add support of user sessions
    server.js Version 0.1.2
    - Add logic for user sessions
    
    views/index.html
    - Mustache rendering correction
    
    views/login.html
    - Add post method for '/loginn' action

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 /**
  * File: server.js
  * @author Theo Technicguy, Sorio
- * @version 0.1.1
+ * @version 0.1.2
  */
 
 // Setup server
@@ -12,6 +12,12 @@ const consolidate = require("consolidate");
 const mongo = require("mongodb").MongoClient;
 const app = express();
 const url = "mongodb://localhost:27017/";
+var https = require('https');
+var fs = require('fs');
+var session = require('express-session');
+const req = require("express/lib/request");
+
+
 app.use(express.urlencoded({
     extended: true
   }));
@@ -22,17 +28,30 @@ app.disable("x-powered-by");
 // Set rendering engine and views
 app.engine("html", consolidate.hogan);
 app.set("views", "views");
+
+//express Sessions
+app.use(session({
+    secret: "propre123",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { 
+      path: '/', 
+      httpOnly: true, 
+      maxAge: 3600000
+    }
+  }));
+
 // Set public assets folder
 app.use(express.static(path.join(__dirname, 'public/')));
 
 app.get(["/", "/index", "/index.html"],(req,res,next)=>{
     let user_param;
     // Make user_param empty if no user is supplied.
-    if (req.query.user == null) {
-        user_param = {};
-    } else {
-        user_param = {"user": {"name": req.query.user}};
-    }
+    if (req.session.username==null){
+        user_param ={}
+    } else{
+    user_param = {"user": {"name": req.session.username}};
+    };
     mongo.connect(url,(err,client)=>{
         if (err) throw err;
         var dbo = client.db("users");
@@ -42,43 +61,56 @@ app.get(["/", "/index", "/index.html"],(req,res,next)=>{
                 client.close();
             });
       });
+    });
+
+app.post("/loginn",(req,res)=>{
+    if(req.body.username){
+        req.session.username=req.body.username;
+        res.redirect('/report');
+    }
+    else{
+        res.redirect('/login');
+    }
 });
 
 app.get("/login", (req, res) => {
+
     res.render("login.html");
 });
 
 app.get(["/report", "/new_incident"], (req, res) => {
     let user_param;
     // Make user_param empty if no user is supplied.
-    if (req.query.user == null) {
-        user_param = {};
-    } else {
-        user_param = {"user": {"name": req.query.user}};
-    }
+    if (req.session.username==null){
+        user_param ={}
+    }else{
+    user_param = {"user": {"name": req.session.username}};
+    };
     res.render("new_incident.html",user_param);
 });
 
 app.post("/insert",(req,res)=>{
-    let user_param="utilisateur";
-    var item ={
-        description: req.body.description,
-        adresse: req.body.adresse,
-        date :new Date().toLocaleDateString() ,
-        author:user_param
-    };
+    if (req.session.username==null){res.redirect('/login')}
+    else{
+        let user_param=req.session.username;
+        var item ={
+            description: req.body.description,
+            adresse: req.body.adresse,
+            date :new Date().toLocaleDateString() ,
+            author:user_param
+        };
 
-    mongo.connect(url,(err,client)=>{
-        if (err) throw err;
-        var dbo = client.db("users");
-        dbo.collection("incidents").insertOne(item, function(err, result) {
-          if (err) throw err;
-          console.log(item);
-          client.close();
+        mongo.connect(url,(err,client)=>{
+            if (err) throw err;
+            var dbo = client.db("users");
+            dbo.collection("incidents").insertOne(item, function(err, result) {
+            if (err) throw err;
+            console.log(item);
+            client.close();
+            });
         });
-      });
-    
-      res.redirect('/')
+        res.redirect('/')
+    };
 });
 app.use(express.static("content"));
 app.listen(8080);
