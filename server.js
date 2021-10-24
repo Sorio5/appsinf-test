4368f27bf9398013bce1c3c1023522c3428da7cb
Theo Technicguy <19630890+TheoTechnicguy@users.noreply.github.com>
2021-10-24 12:21:58 +0200

    Clean up and reformat server.js
    server.js Version: 0.1.3
    - Rearrange requires
    - Set all requires to constants
    - Add config reading
    - Update `PORT` and mongodb `url` to use config options
    - Rearrange server setup
    - Clean whitespaces and general file
    - Update server listen to use `PORT` and `HOST`
    - Add *server started* notice

--- server.js
+++ server.js
@@ -1,67 +1,71 @@
 /**
  * File: server.js
  * @author Theo Technicguy, Sorio
- * @version 0.1.2
+ * @version 0.1.3
  */
 
-// Setup server
+// Imports and modules
 const path = require("path");
-
 const express = require("express");
 const consolidate = require("consolidate");
 const mongo = require("mongodb").MongoClient;
-const app = express();
-const url = "mongodb://localhost:27017/";
-var https = require('https');
-var fs = require('fs');
-var session = require('express-session');
+const https = require('https');
+const fs = require('fs');
+const session = require('express-session');
 const req = require("express/lib/request");
 
+const crypto = require("crypto");
 
-app.use(express.urlencoded({
-    extended: true
-  }));
+const config = require("./config/config");
 
-// By default, express sets a tag `X-Powered-By`. Disable this
-app.disable("x-powered-by");
+// --- Config ---
+const HOST = config.HOST;
+const PORT = config.PORT;
+const SECRET = config.SECRET;
+const SALT = config.SALT;
 
+// --- Setup server ---
+const app = express();
 // Set rendering engine and views
 app.engine("html", consolidate.hogan);
 app.set("views", "views");
-
-//express Sessions
-app.use(session({
-    secret: "propre123",
-    resave: false,
-    saveUninitialized: true,
-    cookie: { 
-      path: '/', 
-      httpOnly: true, 
-      maxAge: 3600000
-    }
-  }));
-
 // Set public assets folder
 app.use(express.static(path.join(__dirname, 'public/')));
+// By default, express sets an `X-Powered-By` header tag. Disable this
+app.disable("x-powered-by");
 
-app.get(["/", "/index", "/index.html"],(req,res,next)=>{
+app.use(express.urlencoded({extended: true}));
+
+// Set express Sessions cookies
+app.use(session({
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        path: '/',
+        httpOnly: true,
+        maxAge: 3600000
+    }
+}));
+
+app.get(["/", "/index", "/index.html"], (req, res) => {
     let user_param;
     // Make user_param empty if no user is supplied.
-    if (req.session.username==null){
-        user_param ={}
-    } else{
-    user_param = {"user": {"name": req.session.username}};
-    };
-    mongo.connect(url,(err,client)=>{
+    if (req.session.username == null) {
+        user_param = {}
+    } else {
+        user_param = {"user": {"name": req.session.username}};
+    }
+    mongo.connect(config.DB_URL, (err, client) => {
         if (err) throw err;
         var dbo = client.db("users");
         dbo.collection("incidents").find({}).toArray((err, result) => {
-                if (err)throw err;
-                res.render("index.html", { result, user_param });
-                client.close();
-            });
-      });
+            if (err) throw err;
+            res.render("index.html", {result, user_param});
+            client.close();
+        });
     });
+});
 
 app.post("/loginn",(req,res)=>{
     if(req.body.username){
@@ -74,43 +78,48 @@ app.post("/loginn",(req,res)=>{
 });
 
 app.get("/login", (req, res) => {
-
     res.render("login.html");
 });
 
 app.get(["/report", "/new_incident"], (req, res) => {
     let user_param;
     // Make user_param empty if no user is supplied.
-    if (req.session.username==null){
-        user_param ={}
-    }else{
-    user_param = {"user": {"name": req.session.username}};
-    };
-    res.render("new_incident.html",user_param);
+    if (req.session.username == null) {
+        user_param = {}
+    } else {
+        user_param = {"user": {"name": req.session.username}};
+    }
+
+    res.render("new_incident.html", user_param);
 });
 
-app.post("/insert",(req,res)=>{
-    if (req.session.username==null){res.redirect('/login')}
-    else{
-        let user_param=req.session.username;
-        var item ={
+app.post("/insert", (req, res) => {
+    if (req.session.username == null) {
+        res.redirect('/login')
+    } else {
+        let user_param = req.session.username;
+        var item = {
             description: req.body.description,
             adresse: req.body.adresse,
-            date :new Date().toLocaleDateString() ,
-            author:user_param
+            date: new Date().toLocaleDateString(),
+            author: user_param
         };
 
-        mongo.connect(url,(err,client)=>{
+        mongo.connect(config.DB_URL, (err, client) => {
             if (err) throw err;
             var dbo = client.db("users");
-            dbo.collection("incidents").insertOne(item, function(err, result) {
-            if (err) throw err;
-            console.log(item);
-            client.close();
+            dbo.collection("incidents").insertOne(item, function (err) {
+                if (err) throw err;
+                console.log(item);
+                client.close();
             });
         });
         res.redirect('/')
-    };
+    }
+
 });
+
 app.use(express.static("content"));
-app.listen(8080);
+app.listen(PORT, HOST, () => {
+    console.log(`Started server. Serving http://${HOST}:${PORT}`);
+});
