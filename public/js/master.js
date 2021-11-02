ff52ab653b1cf38b6c103904e01d227053d11188
Nabil <nabil.bourhanzour@student.uclouvain.be>
2021-11-02 20:05:46 +0100

    Add of headers
    public/js/master.js Version 0.0.1
    - Add headers

--- public/js/master.js
+++ public/js/master.js
@@ -1,4 +1,10 @@
-//mobile menu
+/**
+ * File: master.js
+ * @author: Sorio
+ * @version: 0.0.1
+ *
+ */
+//Burger button menu for phone-sized pages
 const burgerIcon = document.querySelector('#burger');
 const navbarMenu = document.querySelector('#nav-links');
 
